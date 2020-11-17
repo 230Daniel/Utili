5f11c5da9b8d3d16704f60f8c69f9026dacb214d
Daniel Baynton <Daniel.Baynton@hotmail.com>
2020-11-17 20:26:44 +0000

    Download required users

--- Utili/Handlers/ShardHandler.cs
+++ Utili/Handlers/ShardHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Timers;
 using Discord;
@@ -9,41 +10,15 @@ namespace Utili.Handlers
 {
     internal class ShardHandler
     {
-        private static int _shardsStarted;
-
-        public static async Task ShardReady(DiscordSocketClient shard)
-        {
-            _ = Task.Run(async () =>
-            {
-                
-            });
-        }
-
+        private static bool _firstAllConnect = true;
         public static async Task ShardConnected(DiscordSocketClient shard)
         {
             _ = Task.Run(async () =>
             {
-                if (_config.FillUserCache)
+                if (_firstAllConnect && _client.Shards.Count(x => x.ConnectionState == ConnectionState.Connected) == _client.Shards.Count)
                 {
-                    foreach (SocketGuild guild in _client.Guilds)
-                    {
-                        await guild.DownloadUsersAsync();
-                    }
+                    _firstAllConnect = false;
 
-                    int notDownloaded = _client.Guilds.Count(x => x.Users.Count != x.MemberCount);
-                    if (notDownloaded > 0)
-                    {
-                        _logger.Log("Connected", $"Users not fully downloaded for {notDownloaded} guilds",
-                            LogSeverity.Warn);
-                    }
-                }
-
-                await shard.SetGameAsync($"{_config.Domain} | .help");
-
-                _shardsStarted += 1;
-
-                if (_shardsStarted == _config.UpperShardId - _config.LowerShardId + 1)
-                {
                     Database.Sharding.UpdateShardStats(_client.Shards.Count,
                         _client.Shards.OrderBy(x => x.ShardId).First().ShardId, _client.Guilds.Count);
 
@@ -51,9 +26,56 @@ namespace Utili.Handlers
                     _shardStatsUpdater.Elapsed += Sharding.Update;
                     _shardStatsUpdater.Start();
 
-                    _shardsStarted = 0;
+                    await DownloadRequiredUsersAsync();
+                    await _client.SetGameAsync($"{_config.Domain} | .help");
+                    StartDownloadTimer();
                 }
             });
+        }
+
+        private static Timer _downloadTimer;
+        private static void StartDownloadTimer()
+        {
+            _downloadTimer?.Dispose();
+
+            _downloadTimer = new Timer(60000);
+            _downloadTimer.Elapsed += DownloadTimer_Elapsed;
+            _downloadTimer.Start();
+        }
+
+        private static bool _currentlyDownloading;
+        private static void DownloadTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if(!_currentlyDownloading) _ = DownloadRequiredUsersAsync();
+        }
+
+        public static async Task DownloadRequiredUsersAsync()
+        {
+            while (_currentlyDownloading) await Task.Delay(5000);
+
+            _currentlyDownloading = true;
+
+            List<ulong> guildIds = new List<ulong>();
+
+            // Role persist enabled
+            guildIds.AddRange(Database.Data.Roles.GetRows().Where(x => x.RolePersist).Select(x => x.GuildId));
+
+            try
+            {
+                foreach (DiscordSocketClient shard in _client.Shards)
+                {
+                    foreach(SocketGuild guild in shard.Guilds.Where(x => x.DownloadedMemberCount < x.MemberCount && guildIds.Contains(x.Id)))
+                    {
+                        // Ratelimit is 120 requests per minute, I believe that this is global over all shards
+
+                        _ = guild.DownloadUsersAsync();
+                        await Task.Delay(900);
+                    }
+                }
+            }
+            catch { }
+
+            _currentlyDownloading = false;
         }
 
         public static async Task Log(LogMessage logMessage)
