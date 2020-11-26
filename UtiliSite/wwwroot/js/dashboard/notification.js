ae5322daa4e8e4cee7ae83e50b2a60c91b5c83c0
isuryadean <67651362+isuryadean@users.noreply.github.com>
2020-11-26 22:45:13 +0000

    9uy87fu6dyr

--- UtiliSite/wwwroot/js/dashboard/notification.js
+++ UtiliSite/wwwroot/js/dashboard/notification.js
@@ -1,4 +1,32 @@
 ﻿function successNotification() {
     document.getElementById("navbar").style.backgroundColor = "#43b581";
-    document.getElementById("hide").setAttribute = "hidden";
+    document.getElementById("hide").setAttribute("hidden", "");
+    document.getElementById("success").removeAttribute("hidden");
+    setTimeout(() => {
+        document.getElementById("navbar").style.backgroundColor = "#fff";
+        document.getElementById("success").setAttribute("hidden", "");
+        document.getElementById("hide").removeAttribute("hidden");
+    }, 2500);
+    
+};
+function errorNotificationTooFast() {
+    document.getElementById("navbar").style.backgroundColor = "#b54343";
+    document.getElementById("hide").setAttribute("hidden", "");
+    document.getElementById("error-too-fast").removeAttribute("hidden");
+    setTimeout(() => {
+        document.getElementById("navbar").style.backgroundColor = "#fff";
+        document.getElementById("error-too-fast").setAttribute("hidden", "");
+        document.getElementById("hide").removeAttribute("hidden");
+    }, 2500);
+};
+
+function errorNotificationFailure() {
+    document.getElementById("navbar").style.backgroundColor = "#b54343";
+    document.getElementById("hide").setAttribute("hidden", "");
+    document.getElementById("error-failure").removeAttribute("hidden");
+    setTimeout(() => {
+        document.getElementById("navbar").style.backgroundColor = "#fff";
+        document.getElementById("error-failure").setAttribute("hidden", "");
+        document.getElementById("hide").removeAttribute("hidden");
+    }, 2500);
 };
