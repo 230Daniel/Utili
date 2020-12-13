7d467922849d0803acf1c4076a3c269994e8d8a0
Daniel Baynton <Daniel.Baynton@hotmail.com>
2020-12-13 17:09:12 +0000

    Dynamic height elements

--- UtiliSite/wwwroot/js/site.js
+++ UtiliSite/wwwroot/js/site.js
@@ -1,4 +1,19 @@
 ﻿
 function onLoad() {
+    sizeDynamicHeight();
     $(".info-hover").tooltip();
+}
+
+$(window).resize(function() {
+    sizeDynamicHeight();
+});
+
+function sizeDynamicHeight() {
+    var vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    var navHeight = $("#navbar").outerHeight();
+
+    var elements = document.getElementsByClassName("dynamic-height");
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].style.height = vh - navHeight + "px";
+    }
 }
