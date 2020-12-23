892ce98a9f7ad6d7393a21b0b7dc15480fa8cfe1
Daniel Baynton <daniel.baynton@hotmail.com>
2020-12-23 00:18:25 +0000

    Fix premium/success

--- UtiliSite/Pages/Premium/Success.cshtml.cs
+++ UtiliSite/Pages/Premium/Success.cshtml.cs
@@ -1,11 +1,32 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Database.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace UtiliSite.Pages.Premium
 {
     public class SuccessModel : PageModel
     {
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
+            AuthDetails auth = await Auth.GetAuthDetailsAsync(HttpContext);
+            ViewData["auth"] = auth;
+            if(!auth.Authenticated) return Forbid();
+
+            await Task.Delay(2000);
+
+            List<SubscriptionsRow> subscriptions = await Subscriptions.GetRowsAsync(userId: auth.User.Id);
+            ViewData["subscriptions"] = subscriptions.Count;
+            ViewData["slots"] = subscriptions.Sum(x => x.Slots);
+
+            if (subscriptions.Count == 0)
+            {
+                return Redirect(HttpContext.Request.Host.ToString());
+            }
+
+            return new PageResult();
         }
     }
 }
