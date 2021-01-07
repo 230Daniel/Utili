72a4bcbecebb926bf445642111f8955997419ca1
Daniel Baynton <daniel.baynton@hotmail.com>
2021-01-07 11:36:16 +0000

    Something?

--- UtiliSite/Pages/Contact.cshtml.cs
+++ UtiliSite/Pages/Contact.cshtml.cs
@@ -1,16 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace UtiliSite.Pages
 {
     public class ContactModel : PageModel
     {
-        public void OnGet()
+        public async Task OnGet()
         {
+            await Auth.GetOptionalAuthDetailsAsync(this);
         }
     }
 }
