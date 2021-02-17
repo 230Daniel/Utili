753479d6b15c6286f16227e57bee3bf2dfab9bad
Daniel Baynton <daniel.baynton@hotmail.com>
2021-02-17 15:06:29 +0000

    Add UtiliBackend

---
+++ UtiliBackend/Controllers/Test.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UtiliBackend.Controllers
+{
+    public class Test : Controller
+    {
+        [HttpGet("dashboard/{guildId}/test")]
+        public ActionResult Index([Required] ulong guildId)
+        {
+            if (!ModelState.IsValid) return new BadRequestResult();
+            return new JsonResult(new TestObject(guildId));
+        }
+    }
+
+    public class TestObject
+    {
+        public ulong GuildId { get; set; }
+        public string Content { get; set; }
+
+        public TestObject(ulong guildId)
+        {
+            GuildId = guildId;
+            Content = "Hello from the other sideeee!";
+        }
+    }
+}
---
+++ UtiliBackend/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace UtiliBackend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
