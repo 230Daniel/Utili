0f29cdfc409ab680f2b14115686c5ae47b3a9a4a
Daniel Baynton <daniel.baynton@hotmail.com>
2021-05-07 15:07:41 +0100

    Refactor event handlers

--- Utili/Services/BotService.cs
+++ Utili/Services/BotService.cs
@@ -96,93 +96,106 @@ namespace Utili.Services
         }
 
         protected override async ValueTask OnReady(ReadyEventArgs e)
-        // Unlike other events, this event will block the gateway
         {
-            _ = _roleCache.Ready(e);
+            _ = Task.Run(async () =>
+            {
+                _ = _roleCache.Ready(e);
+            });
         }
 
         protected override async ValueTask OnMessageReceived(MessageReceivedEventArgs e)
         {
-            await Task.Yield();
+            _ = Task.Run(async () =>
+            {
+                await _messageLogs.MessageReceived(e);
+                if(await _messageFilter.MessageReceived(e)) return;
+                _ = _notices.MessageReceived(e);
+                _ = _voteChannels.MessageReceived(e);
+                _ = _channelMirroring.MessageReceived(e);
+                _ = _autopurge.MessageReceived(e);
+                _ = _inactiveRole.MessageReceived(e);
+            });
             
-            await _messageLogs.MessageReceived(e);
-            if(await _messageFilter.MessageReceived(e)) return;
-            await _notices.MessageReceived(e);
-            await _voteChannels.MessageReceived(e);
-            await _channelMirroring.MessageReceived(e);
-            await _autopurge.MessageReceived(e);
-            await _inactiveRole.MessageReceived(e);
         }
 
         protected override async ValueTask OnMessageUpdated(MessageUpdatedEventArgs e)
         {
-            await Task.Yield();
-            
-            await _messageLogs.MessageUpdated(e);
-            await _autopurge.MessageUpdated(e);
+            _ = Task.Run(async () =>
+            {
+                _ = _messageLogs.MessageUpdated(e);
+                _ = _autopurge.MessageUpdated(e);
+            });
         }
 
         protected override async ValueTask OnMessageDeleted(MessageDeletedEventArgs e)
         {
-            await Task.Yield();
-            
-            await _messageLogs.MessageDeleted(e);
+            _ = Task.Run(async () =>
+            {
+                _ = _messageLogs.MessageDeleted(e);
+            });
         }
     
         protected override async ValueTask OnMessagesDeleted(MessagesDeletedEventArgs e)
         {
-            await Task.Yield();
-            
-            await _messageLogs.MessagesDeleted(e);
+            _ = Task.Run(async () =>
+            {
+                _ = _messageLogs.MessagesDeleted(e);
+            });
         }
 
         protected override async ValueTask OnReactionAdded(ReactionAddedEventArgs e)
         {
-            await Task.Yield();
-            
-            await _reputation.ReactionAdded(e);
+            _ = Task.Run(async () =>
+            {
+                _ = _reputation.ReactionAdded(e);
+            });
         }
         
         protected override async ValueTask OnReactionRemoved(ReactionRemovedEventArgs e)
         {
-            await Task.Yield();
-            
-            await _reputation.ReactionRemoved(e);
+            _ = Task.Run(async () =>
+            {
+                _ = _reputation.ReactionRemoved(e);
+            });
         }
 
         protected override async ValueTask OnVoiceStateUpdated(VoiceStateUpdatedEventArgs e)
         {
-            await Task.Yield();
-            
-            await _voiceLink.VoiceStateUpdated(e);
-            await _voiceRoles.VoiceStateUpdated(e);
-            await _inactiveRole.VoiceStateUpdated(e);
+            _ = Task.Run(async () =>
+            {
+                _ = _voiceLink.VoiceStateUpdated(e);
+                _ = _voiceRoles.VoiceStateUpdated(e);
+                _ = _inactiveRole.VoiceStateUpdated(e);
+            });
         }
 
         protected override async ValueTask OnMemberJoined(MemberJoinedEventArgs e)
         {
-            await Task.Yield();
-            
-            await _rolePersist.MemberJoined(e);
-            await _joinMessage.MemberJoined(e);
-            await _joinRoles.MemberJoined(e);
+            _ = Task.Run(async () =>
+            {
+                _ = _rolePersist.MemberJoined(e);
+                _ = _joinMessage.MemberJoined(e);
+                _ = _joinRoles.MemberJoined(e);
+            });
         }
 
         protected override async ValueTask OnMemberUpdated(MemberUpdatedEventArgs e)
         {
-            await Task.Yield();
-            
-            await _joinRoles.MemberUpdated(e);
-            await _roleLinking.MemberUpdated(e);
-            await _roleCache.MemberUpdated(e);
+            _ = Task.Run(async () =>
+            {
+                await _joinRoles.MemberUpdated(e);
+                await _roleLinking.MemberUpdated(e);
+                _ = _roleCache.MemberUpdated(e);
+            });
         }
 
         protected override async ValueTask OnMemberLeft(MemberLeftEventArgs e)
         {
-            await Task.Yield();
-            
-            await _rolePersist.MemberLeft(e);
-            await _roleCache.MemberLeft(e);
+            _ = Task.Run(async () =>
+            {
+                await _rolePersist.MemberLeft(e);
+                _ = _roleCache.MemberLeft(e);
+            });
         }
     }
 }
