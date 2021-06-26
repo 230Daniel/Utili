fcbc8395ce1dbaf2a886fbca9b6d06217f9a1c7a
Daniel Baynton <daniel.baynton@hotmail.com>
2021-06-26 16:09:50 +0100

    Add ratelimit to api

--- UtiliBackend/Startup.cs
+++ UtiliBackend/Startup.cs
@@ -89,7 +89,7 @@ namespace UtiliBackend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //app.UseIpRateLimiting();
+            app.UseIpRateLimiting();
 
             if (env.IsDevelopment()) 
                 app.UseDeveloperExceptionPage();
