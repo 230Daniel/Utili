9622e361abe412138db27c4c428f66c4a4dd4de2
Daniel Baynton <daniel.baynton@hotmail.com>
2021-08-05 09:56:28 +0100

    Voice roles

--- src/pages/dashboard/voiceroles.js
+++ src/pages/dashboard/voiceroles.js
@@ -27,7 +27,7 @@ class VoiceRoles extends React.Component{
 
 	render(){
 		var channels = this.state.voiceChannels?.map(x => {return {id: x.id, value: x.name}});
-		var voiceRolesChannels = channels?.filter(x => this.state.voiceRoles?.rows.some(y => y.channelId == x.id));
+		var voiceRolesChannels = channels?.filter(x => this.state.voiceRoles?.some(y => y.channelId == x.id));
 		var roles = this.state.roles?.map(x => {return {id: x.id, value: x.name}});
 		return(
 			<>
@@ -48,7 +48,7 @@ class VoiceRoles extends React.Component{
 								ref={this.settings.channelAdder}/>
 						</Card>
 						<div className="inline">
-							{this.state.voiceRoles?.rows.map((row, i) =>{
+							{this.state.voiceRoles?.map((row, i) =>{
 								return(
 									<Card title={row.channelName} size={350} titleSize={150} inputSize={200} key={row.channelId} onChanged={this.props.onChanged} onRemoved={() => this.onChannelRemoved(row.channelId)}>
 										<CardComponent type="select-value" title="Role" values={roles} value={row.roleId} ref={this.settings.channels[i].roleId}></CardComponent>
@@ -63,55 +63,55 @@ class VoiceRoles extends React.Component{
 	}
 	
 	async componentDidMount(){
-		var response = await get(`dashboard/${this.guildId}/voiceRoles`);
+		var response = await get(`dashboard/${this.guildId}/voice-roles`);
 		this.state.voiceRoles = await response?.json();
-		response = await get(`discord/${this.guildId}/channels/voice`);
+		response = await get(`discord/${this.guildId}/voice-channels`);
 		this.state.voiceChannels = await response?.json();
 		this.state.voiceChannels.push({name:"Any other channel", id:"0"});
 		response = await get(`discord/${this.guildId}/roles`);
 		this.state.roles = await response?.json();
 
-		this.state.voiceRoles.rows = this.state.voiceRoles.rows.filter(x => this.state.voiceChannels.some(y => y.id == x.channelId))
-		for(var i = 0; i < this.state.voiceRoles.rows.length; i++){
+		this.state.voiceRoles = this.state.voiceRoles.filter(x => this.state.voiceChannels.some(y => y.id == x.channelId))
+		for(var i = 0; i < this.state.voiceRoles.length; i++){
 			this.settings.channels.push({ roleId: React.createRef() });
-			this.state.voiceRoles.rows[i]["channelName"] = this.getChannelName(this.state.voiceRoles.rows[i].channelId);
+			this.state.voiceRoles[i]["channelName"] = this.getChannelName(this.state.voiceRoles[i].channelId);
 		}
 
-		this.state.voiceRoles.rows.orderBy(x => x.channelName);
+		this.state.voiceRoles.orderBy(x => x.channelName);
 		this.setState({});
 	}
 
 	onChannelAdded(channel){
 		this.settings.channels.push({ roleId: React.createRef() });
-		this.state.voiceRoles.rows.push({
+		this.state.voiceRoles.push({
 			channelId: channel.id,
 			roleId: 0,
 			channelName: this.getChannelName(channel.id)
 		});
-		this.state.voiceRoles.rows.orderBy(x => x.channelName);
+		this.state.voiceRoles.orderBy(x => x.channelName);
 		this.setState({});
 	}
 
 	onChannelRemoved(id){
 		this.settings.channels.pop();
-		this.state.voiceRoles.rows = this.state.voiceRoles.rows.filter(x => x.channelId != id);
+		this.state.voiceRoles = this.state.voiceRoles.filter(x => x.channelId != id);
 		this.setState({});
 		this.props.onChanged();
 	}
 
 	getInput(){
-		var rows = this.state.voiceRoles.rows;
+		var rows = this.state.voiceRoles;
 		for(var i = 0; i < rows.length; i++){
 			var card = this.settings.channels[i];
 			rows[i].roleId = card.roleId.current.getValue();
 		}
-		this.state.voiceRoles.rows = rows;
+		this.state.voiceRoles = rows;
 		this.setState({});
 	}
 
 	async save(){
 		this.getInput();
-		var response = await post(`dashboard/${this.guildId}/voiceRoles`, this.state.voiceRoles);
+		var response = await post(`dashboard/${this.guildId}/voice-roles`, this.state.voiceRoles);
 		return response.ok;
 	}
 
