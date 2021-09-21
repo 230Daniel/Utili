aab324f49264c1caa5c3667de3a48d637b1868f2
Daniel Baynton <daniel.baynton@hotmail.com>
2021-09-21 21:10:13 +0100

    Message filter thread support

--- src/pages/dashboard/messagefilter.js
+++ src/pages/dashboard/messagefilter.js
@@ -66,8 +66,8 @@ class MessageFilter extends React.Component{
 					<div className="dashboard-subtitle">Force a certain type of message in each channel</div>
 					<div className="dashboard-description">
 						<p>Utili will delete any message that doesn't fit the rule for its channel.</p>
-						<p><b>RegEx:</b> A regular expression in C# style which every message must match. (advanced)</p>
 						<p><b>Message:</b> The message that will be sent if someone's message is deleted - Leave blank for a default message</p>
+						<p><b>RegEx:</b> A regular expression in C# style which every message must match. (advanced)</p>
 					</div>
 					<Load loaded={this.state.messageFilter !== null}>
 						<Card onChanged={this.props.onChanged}>
@@ -82,10 +82,11 @@ class MessageFilter extends React.Component{
 						<div className="inline">
 							{this.state.messageFilter?.map((row, i) =>{
 								return(
-									<Card title={row.channelName} size={350} titleSize={150} inputSize={200} key={row.channelId} onChanged={() => this.onChanged()} onRemoved={() => this.onChannelRemoved(row.channelId)}>
+									<Card title={row.channelName} size={400} titleSize={200} inputSize={200} key={row.channelId} onChanged={() => this.onChanged()} onRemoved={() => this.onChannelRemoved(row.channelId)}>
 										<CardComponent title="Mode" type="select-value" value={row.mode} values={modeValues} hideNone ref={this.settings.channels[i].mode}/>
 										<CardComponent title="RegEx (C#)" type="text" value={row.regEx} visible={row.mode & 64} ref={this.settings.channels[i].regEx}/>
 										<CardComponent title="Message" type="text" value={row.deletionMessage} ref={this.settings.channels[i].deletionMessage}/>
+										<CardComponent title="Enforce in threads" type="checkbox" value={row.enforceInThreads} ref={this.settings.channels[i].enforceInThreads}/>
 									</Card>
 								);
 							})}
@@ -104,7 +105,7 @@ class MessageFilter extends React.Component{
 
 		this.state.messageFilter = this.state.messageFilter.filter(x => this.state.textChannels.some(y => y.id == x.channelId))
 		for(var i = 0; i < this.state.messageFilter.length; i++){
-			this.settings.channels.push({ mode: React.createRef(), regEx: React.createRef(), deletionMessage: React.createRef() });
+			this.settings.channels.push({ mode: React.createRef(), regEx: React.createRef(), deletionMessage: React.createRef(), enforceInThreads: React.createRef() });
 			this.state.messageFilter[i]["channelName"] = this.getChannelName(this.state.messageFilter[i].channelId);
 		}
 		this.state.messageFilter.orderBy(x => x.channelName);
@@ -118,12 +119,13 @@ class MessageFilter extends React.Component{
 	}
 
 	onChannelAdded(channel){
-		this.settings.channels.push({ mode: React.createRef(), regEx: React.createRef(), deletionMessage: React.createRef() });
+		this.settings.channels.push({ mode: React.createRef(), regEx: React.createRef(), deletionMessage: React.createRef(), enforceInThreads: React.createRef() });
 		this.state.messageFilter.push({
 			channelId: channel.id,
 			mode: 1,
 			regEx: "",
 			deletionMessage: "",
+			enforceInThreads: false,
 			channelName: this.getChannelName(channel.id)
 		});
 		this.state.messageFilter.orderBy(x => x.channelName);
@@ -144,6 +146,7 @@ class MessageFilter extends React.Component{
 			rows[i].mode = card.mode.current.getValue();
 			rows[i].regEx = card.regEx.current.getValue();
 			rows[i].deletionMessage = card.deletionMessage.current.getValue();
+			rows[i].enforceInThreads = card.enforceInThreads.current.getValue();
 		}
 		this.state.messageFilter = rows;
 		this.setState({});
