f3e4d6700c5c5f980e2d78cd8ef174a2e2010cc2
Daniel Baynton <daniel.baynton@hotmail.com>
2021-11-07 13:58:16 +0000

    Added redirect from root to frontend

---
+++ UtiliBackend/Controllers/RedirectController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace UtiliBackend.Controllers
+{
+    public class RedirectController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public RedirectController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Redirect($"{_configuration["Frontend:Origin"]}");
+        }
+    }
+}
