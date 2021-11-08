91216ecad002e173a7700df840b7fc1751ac739a
Daniel Baynton <daniel.baynton@hotmail.com>
2021-11-08 15:57:21 +0000

    Get rid of stripe tracking

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import Document from "./pages/document";
 
 import DashboardIndex from "./pages/dashboard/index";
 
-import {getClientId} from "./api/auth";
+import { getClientId } from "./api/auth";
 import PremiumServers from "./pages/premium/servers";
 import PremiumThankYou from "./pages/premium/thankyou";
 
@@ -26,24 +26,24 @@ ReactDOM.render(
 		<>
 			<Switch>
 				<Route path="/dashboard/*">
-					<DashboardLayout/>
+					<DashboardLayout />
 				</Route>
 				<Route path="*">
 					<Layout>
 						<Switch>
-							<Route exact path="/" component={Index}/>
-							<Route exact path="/commands/" component={Commands}/>
-							<Route exact path="/dashboard/" component={DashboardIndex}/>
-							<Route exact path="/premium/" component={Premium}/>
-							<Route exact path="/premium/servers" component={PremiumServers}/>
-							<Route exact path="/premium/thankyou" component={PremiumThankYou}/>
-							<Route exact path="/premium/customerportal" component={CustomerPortal}/>
-							<Route exact path="/premium/checkout/:currency/:slots" component={Checkout}/>
-							<Route exact path="/contact/" component={Contact}/>
-							<Route exact path="/return/" render={() => Return()}/>
-							<Route exact path="/invite/" component={Invite}/>
-							<Route exact path="/invite/:guildId" component={Invite}/>
-							<Route path="/:document" component={Document}/>
+							<Route exact path="/" component={Index} />
+							<Route exact path="/commands/" component={Commands} />
+							<Route exact path="/dashboard/" component={DashboardIndex} />
+							<Route exact path="/premium/" component={Premium} />
+							<Route exact path="/premium/servers" component={PremiumServers} />
+							<Route exact path="/premium/thankyou" component={PremiumThankYou} />
+							<Route exact path="/premium/customerportal" component={CustomerPortal} />
+							<Route exact path="/premium/checkout/:currency/:slots" component={Checkout} />
+							<Route exact path="/contact/" component={Contact} />
+							<Route exact path="/return/" render={() => Return()} />
+							<Route exact path="/invite/" component={Invite} />
+							<Route exact path="/invite/:guildId" component={Invite} />
+							<Route path="/:document" component={Document} />
 						</Switch>
 					</Layout>
 				</Route>
@@ -53,19 +53,19 @@ ReactDOM.render(
 	document.getElementById("root")
 );
 
-function Return(){
+function Return() {
 	const cookies = new Cookies();
 	window.location.search = "";
-	if(!window.location.href.includes("error")){
+	if (!window.location.href.includes("error")) {
 		var returnPath = cookies.get("return_path");
-		if(returnPath){
+		if (returnPath) {
 			window.location.href = `${window.location.origin}${returnPath}`;
 			return;
 		}
 	}
-	else{
+	else {
 		var returnPath = cookies.get("return_path_error");
-		if(returnPath){
+		if (returnPath) {
 			window.location.href = `${window.location.origin}${returnPath}`;
 			return;
 		}
@@ -73,40 +73,40 @@ function Return(){
 	window.location.href = `${window.location.origin}`;
 }
 
-function Invite(props){
+function Invite(props) {
 	var { guildId } = useParams();
 
 	const cookies = new Cookies();
-	cookies.set("return_path", `/dashboard/${guildId}`, { path: "/", maxAge: 60, sameSite: "strict" } );
-	cookies.set("return_path_error", `/dashboard`, { path: "/", maxAge: 60, sameSite: "strict" } );
+	cookies.set("return_path", `/dashboard/${guildId}`, { path: "/", maxAge: 60, sameSite: "strict" });
+	cookies.set("return_path_error", `/dashboard`, { path: "/", maxAge: 60, sameSite: "strict" });
 
-	var url = 	"https://discord.com/api/oauth2/authorize?permissions=8&scope=bot&response_type=code" +
-              	`&client_id=${getClientId()}` +
-				`&redirect_uri=${encodeURIComponent(window.location.origin)}%2Freturn`;
-	
-	if(guildId) url += `&guild_id=${guildId}`;
+	var url = "https://discord.com/api/oauth2/authorize?permissions=8&scope=bot&response_type=code" +
+		`&client_id=${getClientId()}` +
+		`&redirect_uri=${encodeURIComponent(window.location.origin)}%2Freturn`;
+
+	if (guildId) url += `&guild_id=${guildId}`;
 	window.location.href = url;
 	return null;
 }
 
-function defineExtensions(){
+function defineExtensions() {
 	Object.defineProperty(Array.prototype, "orderBy", {
-		value: function orderBy(selector){
+		value: function orderBy(selector) {
 
-			var sorted = []
+			var sorted = [];
 			var count = this.length;
-			for(var i = 0; i < count; i++)
+			for (var i = 0; i < count; i++)
 				sorted.push(selector(this[i]));
 			sorted = sorted.sort();
-			
-			var reconstructed = []
-			for(var i = 0; i < count; i++){
+
+			var reconstructed = [];
+			for (var i = 0; i < count; i++) {
 				var index = this.findIndex(x => selector(x) == sorted[i]);
 				reconstructed.push(this[index]);
 				this.splice(index, 1);
 			}
-				
-			for(var i = 0; i < count; i++){
+
+			for (var i = 0; i < count; i++) {
 				this.push(reconstructed[i]);
 			}
 			return reconstructed;
@@ -116,22 +116,22 @@ function defineExtensions(){
 	});
 
 	Object.defineProperty(Array.prototype, "orderByInt", {
-		value: function orderByInt(selector){
+		value: function orderByInt(selector) {
 
-			var sorted = []
+			var sorted = [];
 			var count = this.length;
-			for(var i = 0; i < count; i++)
+			for (var i = 0; i < count; i++)
 				sorted.push(+selector(this[i]));
 			sorted = sorted.sort((a, b) => a - b);
-			
-			var reconstructed = []
-			for(var i = 0; i < count; i++){
+
+			var reconstructed = [];
+			for (var i = 0; i < count; i++) {
 				var index = this.findIndex(x => +selector(x) == sorted[i]);
 				reconstructed.push(this[index]);
 				this.splice(index, 1);
 			}
-			
-			for(var i = 0; i < count; i++){
+
+			for (var i = 0; i < count; i++) {
 				this.push(reconstructed[i]);
 			}
 			return reconstructed;
--- src/pages/premium/checkout.js
+++ src/pages/premium/checkout.js
@@ -3,35 +3,32 @@ import Load from "../../components/load";
 import { get, post } from "../../api/auth";
 import { withRouter } from "react-router";
 
-class CustomerPortal extends React.Component{
-	constructor(props){
+class CustomerPortal extends React.Component {
+	constructor(props) {
 		super(props);
 		this.currency = this.props.match.params.currency;
 		this.slots = parseInt(this.props.match.params.slots);
-		this.stripe = process.env.NODE_ENV == "production"
-			? window.Stripe("pk_live_51Hcvk4B8DUEVWcSDwjMf0bvWv4NiSZizxfj495VdwB3UvqPZCNYt30781RdZ4tG8QnylVc98ywuj7k13wAec6cCq00I21LkJCn")
-			: window.Stripe("pk_test_51Hcvk4B8DUEVWcSDhAutXkeJErW0lmmZvTahVkIxQij2cNun9JXuh3FfIt2QXlOQVO519maTYUn8V0tcT4fnuvMH000mz5kD2V")
-	}
-	
-	render(){
-		return(
-			<>
-				<h1>Redirecting...</h1>
-				<Load loaded={false}/>
-			</>
-		);
-		
 	}
 
-	async componentDidMount(){
+	render() {
+		return (
+			<>
+				<h1>Redirecting...</h1>
+				<Load loaded={false} />
+			</>
+		);
+
+	}
+
+	async componentDidMount() {
 		var priceId = this.getPriceId(this.currency, this.slots);
 		var response = await post("stripe/create-checkout-session", { priceId: priceId });
 		var responseJson = await response.json();
-		await this.stripe.redirectToCheckout({sessionId: responseJson.sessionId});
+		await window.stripe.redirectToCheckout({ sessionId: responseJson.sessionId });
 	}
 
-	getPriceId(currency, slots){
-		switch(process.env.NODE_ENV){
+	getPriceId(currency, slots) {
+		switch (process.env.NODE_ENV) {
 			case "production":
 				return this.getLivePriceId(currency, slots);
 			case "development":
@@ -40,20 +37,20 @@ class CustomerPortal extends React.Component{
 		}
 	}
 
-	getLivePriceId(currency, slots){
+	getLivePriceId(currency, slots) {
 		switch (slots) {
 			case 1:
 				if (currency === "gbp") return "price_1I9tE0B8DUEVWcSDmXZo0tHg";
 				if (currency === "usd") return "price_1I9tE0B8DUEVWcSDS7A4O1Yo";
 				if (currency === "eur") return "price_1I9tE0B8DUEVWcSDt0axJ6Jy";
 				break;
-	
+
 			case 3:
 				if (currency === "gbp") return "price_1I9tFEB8DUEVWcSDeiZ30gkH";
 				if (currency === "usd") return "price_1I9tFEB8DUEVWcSDdGJrifkV";
 				if (currency === "eur") return "price_1I9tFEB8DUEVWcSDyTu8P9hn";
 				break;
-	
+
 			case 5:
 				if (currency === "gbp") return "price_1I9tJiB8DUEVWcSD23Zy6gBD";
 				if (currency === "usd") return "price_1I9tJiB8DUEVWcSD7pIVqJJ3";
@@ -65,20 +62,20 @@ class CustomerPortal extends React.Component{
 		}
 	}
 
-	getTestPriceId(currency, slots){
+	getTestPriceId(currency, slots) {
 		switch (slots) {
 			case 1:
 				if (currency === "gbp") return "price_1I0bUCB8DUEVWcSDYIEsUvWA";
 				if (currency === "usd") return "price_1I0bUeB8DUEVWcSD6ksIYGra";
 				if (currency === "eur") return "price_1I0bWFB8DUEVWcSDut7TNTEJ";
 				break;
-	
+
 			case 3:
 				if (currency === "gbp") return "price_1I0bb9B8DUEVWcSDduM316AQ";
 				if (currency === "usd") return "price_1I0bcoB8DUEVWcSDuzcORFfl";
 				if (currency === "eur") return "price_1I0bdNB8DUEVWcSDpBaFzZMm";
 				break;
-	
+
 			case 5:
 				if (currency === "gbp") return "price_1I0bcOB8DUEVWcSDgqZAKvV1";
 				if (currency === "usd") return "price_1I0cR7B8DUEVWcSDnikkbgGr";
