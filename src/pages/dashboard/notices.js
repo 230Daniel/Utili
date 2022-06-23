35ed155c9a362e2ef913e5cfd01e6b2942868eeb
Daniel Baynton <daniel.baynton@hotmail.com>
2022-06-23 11:50:44 +0100

    Added sticky notice Pin option

--- src/pages/dashboard/notices.js
+++ src/pages/dashboard/notices.js
@@ -10,8 +10,8 @@ import Card from "../../components/dashboard/card";
 import CardComponent from "../../components/dashboard/cardComponent";
 import CardAdderComponent from "../../components/dashboard/cardAdderComponent";
 
-class Notices extends React.Component{
-	constructor(props){
+class Notices extends React.Component {
+	constructor(props) {
 		super(props);
 		this.guildId = this.props.match.params.guildId;
 		this.state = {
@@ -22,13 +22,13 @@ class Notices extends React.Component{
 		this.settings = {
 			channelAdder: React.createRef(),
 			channels: []
-		}
+		};
 	}
 
-	render(){
-		var channels = this.state.textChannels?.map(x => {return {id: x.id, value: x.name}});
+	render() {
+		var channels = this.state.textChannels?.map(x => { return { id: x.id, value: x.name }; });
 		var noticesChannels = channels?.filter(x => this.state.notices?.some(y => y.channelId == x.id));
-		return(
+		return (
 			<>
 				<Helmet>
 					<title>Sticky Notices - Utili Dashboard</title>
@@ -48,28 +48,29 @@ class Notices extends React.Component{
 					</div>
 					<Load loaded={this.state.notices !== null}>
 						<Card onChanged={this.props.onChanged}>
-							<CardAdderComponent 
-								prompt="Add a channel..." 
-								values={channels} 
+							<CardAdderComponent
+								prompt="Add a channel..."
+								values={channels}
 								selected={noticesChannels}
-								onSelected={(id) => this.onChannelAdded(id)} 
+								onSelected={(id) => this.onChannelAdded(id)}
 								onUnselected={(id) => this.onChannelRemoved(id)}
-								ref={this.settings.channelAdder}/>
+								ref={this.settings.channelAdder} />
 						</Card>
 						<div className="inline">
-							{this.state.notices?.map((row, i) =>{
-								return(
+							{this.state.notices?.map((row, i) => {
+								return (
 									<Card title={row.channelName} size={600} titleSize={200} inputSize={400} key={row.channelId} onChanged={() => this.onCardChanged(i)} onRemoved={() => this.onChannelRemoved(row.channelId)}>
-										<CardComponent type="checkbox" title="Enabled" value={row.enabled} ref={this.settings.channels[i].enabled}/>
-										<CardComponent type="timespan" title="Delay" value={Duration.fromISO(row.delay)} ref={this.settings.channels[i].delay}/>
-										<CardComponent type="text" title="Title" value={row.title} ref={this.settings.channels[i].title}/>
-										<CardComponent type="text-multiline" title="Content" height={80} padding={16} value={row.content} ref={this.settings.channels[i].content}/>
-										<CardComponent type="text" title="Footer" value={row.footer} ref={this.settings.channels[i].footer}/>
-										<CardComponent type="text-multiline" title="Plain Text" height={80} padding={16} value={row.text} ref={this.settings.channels[i].text}/>
-										<CardComponent type="text" title="Image Url" value={row.image} ref={this.settings.channels[i].image}/>
-										<CardComponent type="text" title="Thumbnail Url" value={row.thumbnail} ref={this.settings.channels[i].thumbnail}/>
-										<CardComponent type="text" title="Icon Url" value={row.icon} ref={this.settings.channels[i].icon}/>
-										<CardComponent type="colour" title="Colour" value={row.colour} ref={this.settings.channels[i].colour}/>
+										<CardComponent type="checkbox" title="Enabled" value={row.enabled} ref={this.settings.channels[i].enabled} />
+										<CardComponent type="timespan" title="Delay" value={Duration.fromISO(row.delay)} ref={this.settings.channels[i].delay} />
+										<CardComponent type="checkbox" title="Pin Message" value={row.pin} ref={this.settings.channels[i].pin} />
+										<CardComponent type="text" title="Title" value={row.title} ref={this.settings.channels[i].title} />
+										<CardComponent type="text-multiline" title="Content" height={80} padding={16} value={row.content} ref={this.settings.channels[i].content} />
+										<CardComponent type="text" title="Footer" value={row.footer} ref={this.settings.channels[i].footer} />
+										<CardComponent type="text-multiline" title="Plain Text" height={80} padding={16} value={row.text} ref={this.settings.channels[i].text} />
+										<CardComponent type="text" title="Image Url" value={row.image} ref={this.settings.channels[i].image} />
+										<CardComponent type="text" title="Thumbnail Url" value={row.thumbnail} ref={this.settings.channels[i].thumbnail} />
+										<CardComponent type="text" title="Icon Url" value={row.icon} ref={this.settings.channels[i].icon} />
+										<CardComponent type="colour" title="Colour" value={row.colour} ref={this.settings.channels[i].colour} />
 									</Card>
 								);
 							})}
@@ -79,18 +80,19 @@ class Notices extends React.Component{
 			</>
 		);
 	}
-	
-	async componentDidMount(){
+
+	async componentDidMount() {
 		var response = await get(`dashboard/${this.guildId}/notices`);
 		this.state.notices = await response?.json();
 		response = await get(`discord/${this.guildId}/text-channels`);
 		this.state.textChannels = await response?.json();
 
-		this.state.notices = this.state.notices.filter(x => this.state.textChannels.some(y => y.id == x.channelId))
-		for(var i = 0; i < this.state.notices.length; i++){
-			this.settings.channels.push({ 
+		this.state.notices = this.state.notices.filter(x => this.state.textChannels.some(y => y.id == x.channelId));
+		for (var i = 0; i < this.state.notices.length; i++) {
+			this.settings.channels.push({
 				enabled: React.createRef(),
 				delay: React.createRef(),
+				pin: React.createRef(),
 				title: React.createRef(),
 				content: React.createRef(),
 				footer: React.createRef(),
@@ -106,15 +108,16 @@ class Notices extends React.Component{
 		this.setState({});
 	}
 
-	onCardChanged(i){
+	onCardChanged(i) {
 		this.state.notices[i].changed = true;
 		this.props.onChanged();
 	}
 
-	onChannelAdded(channel){
+	onChannelAdded(channel) {
 		this.settings.channels.push({
 			enabled: React.createRef(),
 			delay: React.createRef(),
+			pin: React.createRef(),
 			title: React.createRef(),
 			content: React.createRef(),
 			footer: React.createRef(),
@@ -123,11 +126,12 @@ class Notices extends React.Component{
 			thumbnail: React.createRef(),
 			icon: React.createRef(),
 			colour: React.createRef()
-		 });
+		});
 		this.state.notices.push({
 			channelId: channel.id,
 			enabled: false,
 			delay: "PT5M",
+			pin: true,
 			title: "",
 			content: "",
 			footer: "",
@@ -142,19 +146,20 @@ class Notices extends React.Component{
 		this.setState({});
 	}
 
-	onChannelRemoved(id){
+	onChannelRemoved(id) {
 		this.settings.channels.pop();
 		this.state.notices = this.state.notices.filter(x => x.channelId != id);
 		this.setState({});
 		this.props.onChanged();
 	}
 
-	getInput(){
+	getInput() {
 		var rows = this.state.notices;
-		for(var i = 0; i < rows.length; i++){
+		for (var i = 0; i < rows.length; i++) {
 			var card = this.settings.channels[i];
 			rows[i].enabled = card.enabled.current.getValue();
 			rows[i].delay = card.delay.current.getValue();
+			rows[i].pin = card.pin.current.getValue();
 			rows[i].title = card.title.current.getValue();
 			rows[i].content = card.content.current.getValue();
 			rows[i].footer = card.footer.current.getValue();
@@ -168,16 +173,16 @@ class Notices extends React.Component{
 		this.setState({});
 	}
 
-	async save(){
+	async save() {
 		this.getInput();
 		var response = await post(`dashboard/${this.guildId}/notices`, this.state.notices);
-		for(var i = 0; i < this.state.notices.length; i++){
+		for (var i = 0; i < this.state.notices.length; i++) {
 			this.state.notices[i].changed = false;
 		}
 		return response.ok;
 	}
 
-	getChannelName(id){
+	getChannelName(id) {
 		return this.state.textChannels.find(x => x.id == id).name;
 	}
 }
