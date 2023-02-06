de02177f571fb66310e07661e31e089cabcec679
Daniel Baynton <daniel.baynton@hotmail.com>
2023-02-06 20:48:27 +0000

    Ammend HasteBin

--- hastebin.sh
+++ hastebin.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-git clone https://github.com/230Daniel/haste-server src/Hastebin
+# Script to download Hastebin and configure it for use with Docker
+
+mkdir hastebin-data
+chmod 777 hastebin-data
+cd src/Hastebin || git clone https://github.com/230Daniel/haste-server src/Hastebin && cd src/Hastebin
+git pull
