734fea87a73fbea9af67995df1667998c2719912
230Daniel <daniel.baynton@hotmail.com>
2023-02-07 13:03:55 -0500

    Configure HasteBin

--- hastebin.sh
+++ hastebin.sh
@@ -2,7 +2,5 @@
 
 # Script to download Hastebin and configure it for use with Docker
 
-mkdir hastebin-data
-chmod 777 hastebin-data
 cd src/Hastebin || git clone https://github.com/230Daniel/haste-server src/Hastebin && cd src/Hastebin
 git pull
